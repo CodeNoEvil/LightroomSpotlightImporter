7461381cc62a19d93c7b023ad9c8fbef31fab6cc
David B. Bitton <david@codenoevil.com>
2012-05-02 01:19:23 -0400

    success with frameworks

---
+++ LightroomImporter/LSICallbackState.h
@@ -0,0 +1,13 @@
+//
+//  LSICallbackState.h
+//  LightroomSpotlightImporter
+//
+//  Created by David Bitton on 5/1/12.
+//  Copyright (c) 2012 Supreme Security Systems, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LSICallbackState : NSObject
+
+@end
---
+++ LightroomImporter/LSICallbackState.m
@@ -0,0 +1,13 @@
+//
+//  LSICallbackState.m
+//  LightroomSpotlightImporter
+//
+//  Created by David Bitton on 5/1/12.
+//  Copyright (c) 2012 Supreme Security Systems, Inc. All rights reserved.
+//
+
+#import "LSICallbackState.h"
+
+@implementation LSICallbackState
+
+@end
